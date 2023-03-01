a48febd374a816cd43ff9d58c5f582be3d78df3c
Ashwin Chavan <86465817+Ash7540@users.noreply.github.com>
2023-03-01 20:59:50 +0530

    Delete function.py

--- function.py
+++
@@ -1,9 +0,0 @@
-def is_leap(year):
-    lp = False
-
-    # Write your logic here
-    if (year % 100 == 0) and (year % 400 != 0):
-        lp = False
-    elif (year % 4 == 0):
-        lp = True
-    return lp
